4c54b0916486e870ada3008025adb70596ad2961
sunnychng <60238115+sunnychng@users.noreply.github.com>
2020-04-16 22:45:27 -0500

    Add new ingredients & new recipes input method
    Add new ingredients & new recipes input method

--- src/main/java/dmacc/controller/WebController.java
+++ src/main/java/dmacc/controller/WebController.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import dmacc.beans.Ingredients;
+import dmacc.beans.Recipes;
 import dmacc.repository.MealPlanRepository;
 
 @Controller
@@ -23,5 +25,19 @@ public class WebController {
 		return "recipeView";
 	}
 	
-
+	// Ingredients input
+	@GetMapping("/inputIngrediants")
+	public String addNewIngredients(Model model) {
+		Ingredients i = new Ingredients();
+		model.addAttribute("newIngrediants", i);
+		return "inputNewIngredients";
+	}
+	
+	// Recipes input
+	@GetMapping("/inputRecipes")
+	public String addNewRecipes(Model model) {
+		Recipes r = new Recipes();
+		model.addAttribute("newRecipes", r);
+		return "inputNewRecipes";
+	}
 }
